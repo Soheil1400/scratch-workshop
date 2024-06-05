c2bc9d7fdceeadd707429eb21f1991fca1fe2037
soheil <soheil.saedi1379@gmail.com>
2024-06-05 17:26:08 +0330

    second commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,14 +6,11 @@
 	<div class="w-full max-w-6xl p-16 mx-auto bg-white rounded-2xl">
 		<header class="mx-auto mb-20 max-w-prose drop-shadow-sm">
 			<h1 class="mb-2 text-6xl font-bold text-purple-800">
-				<a href="/">Scratch for Kids</a>
+				<a href="/">Scratch</a>
 			</h1>
-			<p>
-				A <a
-					href="https://frontendmasters.com/courses/scratch/"
-					target="_blank"
-					class="font-semibold underline decoration-purple-600 decoration-2">Watch the course on Frontend Masters</a>
-			</p>
+			<h1 class="mb-2 text-1xl font-bold text-black-800">
+			    سهیل ساعدی فرد
+			</h1>
 		</header>
 		<main class="mx-auto prose prose-pre:bg-slate-100">
 			<slot />
--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -31,15 +31,7 @@
 	</div>
 	<footer class="flex justify-between mt-16 border-t-2 border-slate-200">
 		<p>
-			<a href="/" class="text-purple-800 underline decoration-black">&larr; Back</a>
-		</p>
-		<p>
-			<span class="font-semibold">Something look off?</span> Please feel free to submit
-			<a
-				href="https://github.com/stevekinney/scratch-workshop/blob/main/src/content/{data.slug}.md"
-				target="_blank"
-				class="text-purple-800 underline decoration-black">a pull request</a
-			>.
+			<a href="/" class="text-purple-800 underline decoration-black">&larr; برگشت</a>
 		</p>
 	</footer>
 </article>
